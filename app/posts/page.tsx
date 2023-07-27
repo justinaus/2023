c32c028e09cf80ecc967e4ea41b9c647820ba5f4
justinaus <justinaus2k@gmail.com>
2023-07-27 18:33:39 +0900

    Test

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,3 +1,26 @@
-export default function Posts() {
-  return <div>Posts</div>;
+async function getData() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${Math.floor(
+      Math.random() * 100,
+    )}`,
+    {
+      cache: 'no-store',
+    },
+  );
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export default async function Posts() {
+  const data = await getData();
+
+  return <div>{data.body}</div>;
 }
